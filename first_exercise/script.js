b5c564f895ea45cd2f26942632b4a389ed793514
TeddyTheKing <heldinho17@gmail.com>
2023-08-01 22:07:39 +0200

    Finish 1st exercise

--- first_exercise/script.js
+++ first_exercise/script.js
@@ -3,15 +3,20 @@ const container = document.querySelector('#container');
 // creating <p>
 const odst = document.createElement('p');
 odst.textContent = "Hey I'm red!";
-odst.setAttribute('color: red;');
+odst.setAttribute('style', 'color: red;');
+container.append(odst);
 //creating <h3>
 const nadpis = document.createElement('h3');
 nadpis.textContent = "I'm a blue h3!";
-nadpis.setAttribute('color: blue;');
+nadpis.setAttribute('style', 'color: blue;');
+container.append(nadpis);
 // creating <div>
 const slozka = document.createElement('div');
-slozka.setAttribute('border-color: black; background: pink;')
+slozka.setAttribute('style', 'border-color: black; background: pink;')
 const nadpisB = document.createElement('h1');
 const odstB = document.createElement('p');
+nadpisB.textContent = "I'm in a div";
+odstB.textContent = "ME TOO!";
+container.append(slozka);
 slozka.append(nadpisB);
 slozka.append(odstB);
